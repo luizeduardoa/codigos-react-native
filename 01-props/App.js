03e0064144e871f60534165164b8209144d01b19
LUIZ EDUARDO SANTOS DE ALBUQUERQUE <luiz.esalbuquerque@senacsp.edu.br>
2026-08-19 11:48:24 -0300

    App.js

---
+++ 01-props/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native';
+
+class App extends Component {
+
+render(){
+
+    let nome = 'Steve';
+
+    return(
+        <View>
+        <Text>Olá Mundo!</Text>
+        <Text style={{color: '#FF0000', fontSize: 25, margin: 15 }}>Olá Novamente</Text>
+
+        <Text style={{ fontSize: 40 }}>{nome}</Text>
+
+        <Jobs largura={500} altura={200} nomeando="Steve Jobs" />
+        </View>
+    );
+}
+}
+
+export default App;
+
+class Jobs extends Component{
+    render(){
+let image = 'https://blog.ibyte.com.br/wp-content/uploads/2013/09/Steve-jobs-destaque.jpg';
+
+
+return(
+    <View>
+    <Image
+    source={{uri: image }}
+    style={{width: this.props.largura, height: this.props.altura }}
+    />
+
+    <Text>{this.props.nomeando}</Text>
+    </View>
+);
+   
+}
+}

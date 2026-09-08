b1b08c53c4b9eda99ace530f8a662488bbff9ba4
LUIZ EDUARDO SANTOS DE ALBUQUERQUE <luiz.esalbuquerque@senacsp.edu.br>
2026-09-08 08:42:21 -0300

    Http

---
+++ 13-http/src/services/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+                //Bloqueio https no Senac
+       // baseURL: 'https://viacep.com.br/ws'
+
+       baseURL: 'http://viacep.com.br/ws'
+});
+
+export default api;
